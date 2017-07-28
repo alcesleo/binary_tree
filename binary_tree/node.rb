1ed7c38dbb3e292354abd31fb4674353cd8410c4
Jimmy Börjesson <alcesleo@gmail.com>
2017-07-28 11:36:28 +0200

    Refactor breadth first traversal

--- binary_tree/node.rb
+++ binary_tree/node.rb
@@ -4,12 +4,13 @@ class Node
     def right; self; end
     def invert; self; end
     def traverse(_visitor); end
+    def _breadth_first(_visitor, _queue); end
   end
 
   EMPTY = EmptyNode.new
 
   attr_reader :value, :left, :right
-  protected :value, :left, :right
+  private :value, :left, :right
 
   def initialize(value, left = EMPTY, right = EMPTY)
     @value = value
@@ -23,20 +24,21 @@ class Node
     right.traverse(visitor)
   end
 
-  def breadth_first(visitor)
-    queue = [self]
-
-    until queue.empty?
-      current_node = queue.shift
-
-      visitor.call(current_node.value)
-
-      queue.push(current_node.left) unless current_node.left == EMPTY
-      queue.push(current_node.right) unless current_node.right == EMPTY
-    end
-  end
-
   def invert
     Node.new(value, right.invert, left.invert)
+  end
+
+  def breadth_first(visitor)
+    queue = [self]
+    queue.shift._breadth_first(visitor, queue) until queue.empty?
+  end
+
+  protected
+
+  def _breadth_first(visitor, queue)
+    visitor.call(value)
+
+    queue.push(left)
+    queue.push(right)
   end
 end

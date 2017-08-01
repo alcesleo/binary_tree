4a043deac7bc2892647cc3f954035f68c2e586f5
Jimmy Börjesson <alcesleo@gmail.com>
2017-08-01 14:04:35 +0200

    Start from scratch

--- binary_tree/binary_tree_test.rb
+++ binary_tree/binary_tree_test.rb
@@ -2,42 +2,4 @@ require "minitest/autorun"
 require_relative "node"
 
 class BinaryTreeTest < Minitest::Test
-  def setup
-    #      4
-    #    /   \
-    #   2     7
-    #  / \   / \
-    # 1   3 6   9
-    @tree = Node.new(4,
-              Node.new(2,
-                Node.new(1),
-                Node.new(3)),
-              Node.new(7,
-                Node.new(6),
-                Node.new(9)))
-  end
-
-  def test_depth_first
-    assert_tree [4, 2, 1, 3, 7, 6, 9], @tree
-  end
-
-  def test_invert
-    assert_tree [4, 7, 9, 6, 2, 3, 1], @tree.invert
-    assert_tree [4, 2, 1, 3, 7, 6, 9], @tree
-  end
-
-  def test_breadth_first
-    assert_tree [4, 2, 7, 1, 3, 6, 9], @tree, :breadth_first
-  end
-
-  private
-
-  def assert_tree(nodes, tree, traversal_method = :depth_first)
-    result = []
-    visitor = -> (value) { result << value }
-
-    tree.public_send(traversal_method, visitor)
-
-    assert_equal nodes, result
-  end
 end
